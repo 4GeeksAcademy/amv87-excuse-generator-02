4b6ede072528379a38e5957c6bb6e286ab79a5f2
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-10-04 19:51:36 +0000

    now with a function

--- src/app.js
+++ src/app.js
@@ -6,23 +6,26 @@ window.onload = () => {
   document.querySelector("#excuse").innerHTML = generateExcuse();
 };
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function bringPosition(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
 let generateExcuse = () => {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-
-  let whoRandom = Math.floor(Math.random() * who.length);
-  let actionRandom = Math.floor(Math.random() * action.length);
-  let whatRandom = Math.floor(Math.random() * what.length);
-  let whenRandom = Math.floor(Math.random() * when.length);
-
+  let whoRandom = bringPosition(who);
+  let actionRandom = bringPosition(action);
+  let whatRandom = bringPosition(what);
+  let whenRandom = bringPosition(when);
   return (
     who[whoRandom] +
     " " +
